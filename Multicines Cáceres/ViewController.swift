24ea30debbfd150f180a3ef6b141a0e9d6b7e60a
Daniel Llanos Muñoz <daniel.llanosmunoz@gmail.com>
2019-06-05 13:33:38 +0200

    Added mock images

--- Multicines Cáceres/ViewController.swift
+++ Multicines Cáceres/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var carousel: iCarousel!
+    
+    var images = [UIImage(named: "test1"),
+                  UIImage(named: "test2"),
+                  UIImage(named: "test3"),
+                  UIImage(named: "test4"),
+                  UIImage(named: "test5"),
+                  UIImage(named: "test6"),
+                  UIImage(named: "test7"),
+                  UIImage(named: "test8"),
+                  UIImage(named: "test9"),
+                  UIImage(named: "test10")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        carousel.type = .cylinder
+        carousel.contentMode = .scaleAspectFit
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+}
 
+extension ViewController: iCarouselDelegate, iCarouselDataSource{
+    
+    func numberOfItems(in carousel: iCarousel) -> Int {
+        return images.count
+    }
+    
+    func carousel(_ carousel: iCarousel, viewForItemAt index: Int, reusing view: UIView?) -> UIView {
+        var image: UIImageView!
+        
+        if(view==nil){
+            image = UIImageView(frame: CGRect(x: 0, y: 0, width: 250, height: 250))
+            image.contentMode = .scaleAspectFit
+        } else {
+            image = view as? UIImageView
+        }
+        
+        image.image = images[index]
+        return image
+    }
+    
 }
 
